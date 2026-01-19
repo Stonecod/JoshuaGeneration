fa6caa56df4bfc6686d3abc7f81ef7b4b1a853d0
Philip Balogun <olugbayiphilip923@gmail.com>
2026-01-19 19:35:09 +0100

    Add more pages and update background images

--- main.js
+++ main.js
@@ -86,3 +86,26 @@ toggleBtn.addEventListener('click', () => {
     toggleBtn.textContent = 'Show Calendar';
   }
 });
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let currentSection = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    const sectionHeight = section.offsetHeight;
+
+    if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+      currentSection = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${currentSection}`) {
+      link.classList.add("active");
+    }
+  });
+});
